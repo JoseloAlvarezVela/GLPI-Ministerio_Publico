21497e067e08928545694a9883c0f96813b41c1e
joseloAlvarezVela <joseloalvarez18@gmail.com>
2022-08-27 14:45:55 -0500

    commit

--- app/src/main/java/com/glpi/glpi_ministerio_pblico/ui/tickets/TicketsAgregarTareaActivity.kt
+++ app/src/main/java/com/glpi/glpi_ministerio_pblico/ui/tickets/TicketsAgregarTareaActivity.kt
@@ -2,6 +2,7 @@ package com.glpi.glpi_ministerio_pblico.ui.tickets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.isVisible
 import com.glpi.glpi_ministerio_pblico.R
 import com.glpi.glpi_ministerio_pblico.databinding.ActivityTicketsAgregarTareaBinding
@@ -13,9 +14,60 @@ class TicketsAgregarTareaActivity : AppCompatActivity() {
         binding = ActivityTicketsAgregarTareaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        btn_fabs()
+
+
+    }
+
+    private fun btn_fabs(){
+        var click: Boolean = false
+
+        //INICIO DE inicialización de variables
+        val desplegar_fabs = binding.fabDesplegarAddTarea
+        val abrir_plantilla = binding.btnFabPlantilla
+            val layout_plantilla = binding.fabPlantilla
+        val abrir_camara = binding.btnFabFoto
+            val layout_camara = binding.fabFoto
+        val abrir_archivo = binding.btnFabArchivo
+            val layout_archivo = binding.fabArchivo
+        val hide_fab = binding.LayoutFabAgregarTarea
+        val backGround = binding.LayoutBackgroudAgregarTarea
+        //FIN DE inicialización de variables
+
         //INICIO de eventos de click de los FAB's
-        binding.btnFabPlantilla.setOnClickListener {
+        desplegar_fabs.setOnClickListener {
+            if (click == false){
+                layout_plantilla.isVisible = true
+                layout_camara.isVisible = true
+                layout_archivo.isVisible = true
+                backGround.isVisible = true
+                click = true
+            }else{
+                layout_plantilla.isVisible = false
+                layout_camara.isVisible = false
+                layout_archivo.isVisible = false
+                backGround.isVisible = false
+                click = false
+            }
+
+        }
+        abrir_plantilla.setOnClickListener {
             binding.includeModalPlantillaTarea.modalPlantillaAgregarTarea.isVisible = true
+            hide_fab.isVisible = false
+        }
+        abrir_camara.setOnClickListener {
+            Toast.makeText(this, "introduce código que abre la camara del celular", Toast.LENGTH_SHORT).show()
+        }
+        abrir_archivo.setOnClickListener {
+            Toast.makeText(this, "Abrir Archivos", Toast.LENGTH_SHORT).show()
+        }
+        backGround.setOnClickListener {
+            binding.includeModalPlantillaTarea.modalPlantillaAgregarTarea.isVisible = false
+            layout_plantilla.isVisible = false
+            layout_camara.isVisible = false
+            layout_archivo.isVisible = false
+            backGround.isVisible = false
+            binding.LayoutFabAgregarTarea.isVisible = true
         }
         //FIN de eventos de click de los FAB's
     }
