fad53fbe1406e3ebd91bdc705cbc68d52f5ac4f9
joseloAlvarezVela <joseloalvarez18@gmail.com>
2022-08-31 17:24:14 -0500

    commit

--- app/src/main/java/com/glpi/glpi_ministerio_pblico/MainActivity.kt
+++ app/src/main/java/com/glpi/glpi_ministerio_pblico/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_mis_peticiones,R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_mis_peticiones,R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.acttivity_misIncidencias
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/glpi/glpi_ministerio_pblico/ui/misIncidencias/MisIncidenciasActivity.kt
@@ -0,0 +1,14 @@
+package com.glpi.glpi_ministerio_pblico.ui.misIncidencias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.glpi.glpi_ministerio_pblico.databinding.ActivityMisIncidenciasBinding
+
+class MisIncidenciasActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMisIncidenciasBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMisIncidenciasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
